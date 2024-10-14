d902176b8dc5f345d991a938d3ff690291320d4c
Ali Berkay BERBER <aliberkayberber@gmail.com>
2024-10-14 17:57:51 +0300

    added readmme

---
+++ Readme.md
@@ -0,0 +1,5 @@
+## .Net 8 de ms sql , identity framework ve entity framework kullanarak register, sign in, role atama, gibi iþlemlerin yapýldýðý örnek projesidir.
+
+	** Validation
+	** Authentication and Authorization
+	** 
--- obj/Debug/net8.0/IdentityAndDataProtectionExample.AssemblyInfo.cs
+++ obj/Debug/net8.0/IdentityAndDataProtectionExample.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityAndDataProtectionExample")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b5074bf357c78e1b5b95cb46ceeef58b2c426f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityAndDataProtectionExample")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityAndDataProtectionExample")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
